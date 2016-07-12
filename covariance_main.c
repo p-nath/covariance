4561dd4c8546a663ccf471061f0c254eb632b857
Priyanka Nath <pnath.it@gmail.com>
2016-07-13 01:08:25 +0530

    fixed parse_args()

--- covariance_main.c
+++ covariance_main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "matrix.h"
 
-#define FALSE 0
-#define TRUE 1
+void help(char *program_name) {
+  printf("%s -i <input_filename> - o<output_filename>\n", program_name);
+}
 
-int parse_args(int argc, char** argv, char **input_filename, char **output_filename) {
-  if (argc >= 2){
-    *input_filename = argv[1];
-    if (argc == 3)
-      *output_filename = argv[2];
-    return TRUE;
+bool parse_args(int argc, char** argv, char **input_filename, char **output_filename) {
+  if (argc < 3)  return false;
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "-i") == 0) {
+      i++;
+      *input_filename = argv[i];
+    }
+    if (strcmp(argv[i], "-o") == 0) {
+      i++;
+      *output_filename = argv[i];
+    }
   }
-  else {
-    printf("%s <input_filename> <output_filename>\n", argv[0]);
-    return FALSE;
-  }
+  return true;
 }
 
 int main(int argc, char **argv) {
   char *input_filename = NULL, *output_filename = NULL;
-  if (!parse_args(argc, argv, &input_filename, &output_filename))  exit(-1);
+  if (!parse_args(argc, argv, &input_filename, &output_filename)) {
+    help(argv[0]);
+    exit(-1);
+  }
   FILE *fout = NULL;
   if (!(fout = fopen(output_filename,"w")))  fout = stdout;
   Matrix *m1 = NULL, *covariance;
   
   m1 = ReadMatrix(input_filename);
-
   covariance = Covariance(m1);
   WriteMatrix(fout, covariance);
   if (!(fout == stdout)) {
