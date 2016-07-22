286918ee33b00aae6fefa5d53644f0ea5085a48d
Priyanka Nath <pnath.it@gmail.com>
2016-07-22 21:25:07 +0530

    added parse_agrs using argparse library

--- py/covariance_test.py
+++ py/covariance_test.py
@@ -1,11 +1,26 @@
 import numpy as np
+import argparse
+import sys
 
-file = open("input.txt", 'r')
-file.readline() #ignoring the first line
+def parse_args():
+  parser = argparse.ArgumentParser(description="Get input filename.")
+  parser.add_argument("-i", action="store", dest="input_filename", 
+                      default=None, help="Specify input filename.")
+  args = parser.parse_args(sys.argv[1:])
+  return args
 
-arr = [ map(float,line.split(',')) for line in file ]
-X = np.array(arr) #converting list to numpy array
-covariance =  np.cov(X.T)
-print covariance
-np.savetxt('output_py.txt', covariance, fmt = '%.6f')
-file.close()
+def main():
+  args = parse_args()
+  input_filename = args.input_filename
+  file = open(input_filename, 'r')
+  file.readline() #ignoring the first line
+
+  arr = [ map(float,line.split(',')) for line in file ]
+  X = np.array(arr) #converting list to numpy array
+  covariance =  np.cov(X.T)
+  print "Covariance printed on output_py.txt"
+  np.savetxt('output_py.txt', covariance, fmt = '%.6f')
+  file.close()
+
+if __name__ == '__main__':
+  main()
